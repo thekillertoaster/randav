7299614daec8674676106d0b059095e1320d96c2
Aaron Hands <thekillertoaster@gmail.com>
2023-03-25 22:39:30 +0000

    + added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="randav",
+    version="0.1.0",
+    author="Aaron Hands",
+    author_email="thekillertoaster@users.noreply.github.com",
+    description="An advanced random library for Python, with a focus on speed and ease of use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thekillertoaster/randav",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={},
+)
